927109a8834126befc394bb4d6f1cd90e2b1682d
Lincon001 <audomsak.kofak@gmail.com>
2024-12-18 17:46:10 +0700

    update

--- index.php
+++ index.php
@@ -1,8 +1,59 @@
 <h1>File index.php</h1>
-<?php 
-
-/**File index.php */
-# comment
-// comment
-
+<?php
+$my_arr = array(1,2,3,4,5, "myindex" => 6);
+// count($my_array)
+foreach ($my_arr as $key => $value) {
+ echo "<br>";
 ?>
+    <h1><?php echo  $my_var?></h1>
+<?php
+} // end for
+?>
+
+
+
+<?php
+$my_arr = array(1,array(2),3,4,5,"myindex" => (3+3),"myindex" => 7);
+print_r($my_arr);
+echo "<br>";
+$my_arr2[]=1;
+$my_arr2[]= 2;
+$my_arr2[]= 3;
+print_r($my_arr2);
+echo"<br>";
+
+if(false){
+    echo "if fault";
+}else if(TrUE || false){
+    echo "else if true";
+}else {
+    echo "else";
+}
+echo "<br>";
+
+
+
+
+$myvar = "Hello World";
+$myvar = 1;
+echo "<h1>". $myvar ."</h1>";
+echo "<br>";
+$x = 1;
+function x(){
+   global $x;
+   $x = 2;
+
+}
+echo $x;
+echo "<br>";
+x();
+echo $x;
+echo"<br>";
+echo "1.5"+"1";
+echo "<br>";
+$x = "B"; 
+$x++;
+echo $x ;
+echo "<br>"
+?>
+<h1><?php echo $myvar ; ?></h1>
