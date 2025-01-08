d4418ed9ceca74bbba4b4be16baca1a78daba401
Lincon001 <audomsak.kofak@gmail.com>
2025-01-08 18:25:03 +0700

    update

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
+
+Route::get('/mycontroller',
+[MyConTroller::class, 'myfunction']);
+
+Route::post('/mycontroller',
+[MyConTroller::class, 'myfunction']);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return "<h1>Hello World!</h1>";
+Route::get('/hello/{id?}', 
+    function ($val=""){
+    return "<h1>Hello World! $val</h1>";
 });
